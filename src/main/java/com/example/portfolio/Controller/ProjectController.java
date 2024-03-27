01883ea9127291f3d99126181b570d8b804ea12e
unknown <sola2014@naver.com>
2024-03-27 21:33:23 +0900

    feat: 파일 업로드 추가

--- src/main/java/com/example/portfolio/Controller/ProjectController.java
+++ src/main/java/com/example/portfolio/Controller/ProjectController.java
@@ -25,6 +25,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -119,5 +120,13 @@ public class ProjectController {
         List<Project> projects = projectService.categorySearch(categorySearchDto);
 
         return ResponseEntity.ok(projects);
+    }
+
+
+    @PostMapping("/upload")
+    public ResponseEntity<?> uploadImage(@RequestParam("file") MultipartFile file) {
+        System.out.println(file);
+
+        return ResponseEntity.ok(file);
     }
 }
